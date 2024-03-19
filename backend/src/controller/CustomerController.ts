0b9e7246643726f882c616dd58298a4fe16bb244
jeferson-viana <jefersonv28@gmail.com>
2024-03-18 22:34:03 -0300

    FEAT: creating method POST and PUT

--- backend/src/controller/CustomerController.ts
+++ backend/src/controller/CustomerController.ts
@@ -8,4 +8,16 @@ export default class CustomerController {
   const { data } = await this.service.getAllCustomers();
     return res.status(200).json(data);
   }
+
+  public async createCustomer(req: Request, res: Response) {
+    // const { data } = await this.service.createCustomer(req.body.payload);
+    const { data } = await this.service.createCustomer(req.body);
+    return res.status(201).json(data);
+  }
+
+  public async updateCustomer(req: Request, res: Response) {
+    const { data } = await this.service.updateCustomer(req.params.id, req.body);
+    // const { data } = await this.service.updateCustomer(req.params.id, req.body.payload);
+    return res.status(200).json(data);
+  }
 }
